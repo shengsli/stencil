ffdce00eda7b48d44f25bdc6566ed8152bf471c8
shengsli <sheng.s.li@outlook.com>
2019-07-13 22:24:41 +0100

    separate sequential code and parallel code

--- stencil_code/ShengExamples/Median2D/median2D.cpp
+++ stencil_code/ShengExamples/Median2D/median2D.cpp
@@ -1,5 +1,5 @@
 /**
- * g++ median2D.cpp -std=c++11 -O2 -lpthread -DNTHREADS=4 -DNDATABLOCKS=100 -DRADIUS=1 -DNROWS=4 -DNCOLS=5 -DPADDING=0 -DNITERS=4 -DOUTPUT -o median2D
+ * g++ median2D.cpp -std=c++11 -O2 -lpthread -DNTHREADS=4 -DNDATABLOCKS=100 -DRADIUS=1 -DNROWS=4 -DNCOLS=5 -DPADDING=0 -DNITERS=4 -o median2D
  * ./median2D
  */
 
@@ -8,30 +8,11 @@
 
 #include "../../Stencil2D.hpp"
 
-void print_arr(int *arr, int size)
-{
-	int i;
-	for (i=0; i<size-1; i++)
-	{
-		printf("%d, ", arr[i]);
-	}
-	printf("%d\n", arr[i]);
-}
-
 void swap(int *xp, int *yp) 
 {
     int temp = *xp;
     *xp = *yp; 
     *yp = temp;
-} 
-
-void bubble_sort (int *arr, int size) 
-{
-	int i, j;
-	for (i = 0; i<size-1; i++)
-		for (j = 0; j<size-i-1; j++)
-			if (arr[j] > arr[j+1])
-				swap(&arr[j], &arr[j+1]);
 } 
 
 int partition (int arr[], int low, int high)
@@ -67,34 +48,6 @@ int find_median (int *arr, int size)
 	return (arr[(size-1)/2] + arr[size/2])/2;
 }
 
-bool compareResult(const std::vector<int> &vec1, const std::vector<int> &vec2)
-{
-	if (vec1.size() != vec2.size())
-		return false;
-	auto it1 = vec1.begin();
-	auto it2 = vec2.begin();
-	for (; it1 != vec1.end(); ++it1)
-	{
-		if (*it1 != *it2)
-			return false;
-		++it2;
-	}
-	return true;
-}
-
-void printVector(const std::vector<int> &vec)
-{
-	auto it = vec.begin();
-	for (int row=0; row<NROWS; ++row)
-	{
-		for (int col=0; col<NCOLS; ++col)
-		{
-			printf("%4d", vec[col+row*NCOLS]);
-		}
-		std::cout << std::endl;
-	}
-}
-
 double second()
 {
 	struct timeval tp;
@@ -107,80 +60,6 @@ double second()
 int stencilkernel (int neighbourhood[], int radius)
 {
 	return find_median(neighbourhood, (2*radius+1)*(2*radius+1));
-}
-
-void sequentialMedian(std::vector<int> &output, std::vector<int> &input)
-{
-	double tstart, tstop;
-    tstart = second();
-
-	// find median
-	int filterSize = 2*RADIUS+1;
-	int *neighbourhood = (int *) malloc(filterSize*filterSize*sizeof(int));
-	int nItems = NROWS*NCOLS;
-	for (int iter=0; iter<NITERS; iter++) {
-		if (iter>0) { // NEW
-			auto temp = input;
-		    input = output;
-			output = temp;
-		}
-		
-		for (int elIdx=0; elIdx<nItems; ++elIdx) {
-			int elCol = elIdx % NCOLS;
-			int elRow = elIdx / NCOLS;
-			int neighbourCol, neighbourRow;
-			int col, row;
-
-			for (int filterIdx=0; filterIdx<filterSize*filterSize; filterIdx++) { // iterate over filter window, loop coalescing
-				row = filterIdx / filterSize;
-				col = filterIdx % filterSize;
-				switch (PADDING) {
-				    case WRAP_AROUND:
-					{
-						neighbourCol = (elCol+col+NCOLS-RADIUS)%NCOLS;
-						neighbourRow = (elRow+row+NROWS-RADIUS)%NROWS;
-						neighbourhood[col+row*(2*RADIUS+1)] = input[neighbourCol+neighbourRow*NCOLS];
-						break;
-					}
-				    case FIXED_VALUE:
-					{
-						neighbourCol = elCol+col-RADIUS;
-						neighbourRow = elRow+row-RADIUS;
-						if (neighbourCol<0 || neighbourCol>=NCOLS || neighbourRow<0 || neighbourRow>=NROWS)
-							neighbourhood[col+row*(2*RADIUS+1)] = 0;
-						else
-							neighbourhood[col+row*(2*RADIUS+1)] = input[neighbourCol+neighbourRow*NCOLS];
-						break;
-					}
-				    case REPLICATE_LAST_ELEMENT:
-					{
-						neighbourCol = elCol+col-RADIUS;
-						neighbourRow = elRow+row-RADIUS;
-						if ((neighbourCol<0 || neighbourCol>=NCOLS) && (neighbourRow<0 || neighbourRow>=NROWS))
-							neighbourhood[col+row*(2*RADIUS+1)] = 0;
-						else if (neighbourCol<0)
-							neighbourhood[col+row*(2*RADIUS+1)] = input[neighbourRow*NCOLS];
-						else if (neighbourCol>=NCOLS)
-							neighbourhood[col+row*(2*RADIUS+1)] = input[(neighbourRow+1)*NCOLS-1];
-						else if (neighbourRow<0)
-							neighbourhood[col+row*(2*RADIUS+1)] = input[neighbourCol];
-						else if (neighbourRow>=NROWS)
-							neighbourhood[col+row*(2*RADIUS+1)] = input[neighbourCol+(NROWS-1)*NCOLS];
-						else
-							neighbourhood[col+row*(2*RADIUS+1)] = input[neighbourCol+neighbourRow*NCOLS];
-						break;
-					}
-				    default:
-						throw std::invalid_argument("Invalid padding option.");
-						break;
-				}
-			}
-			output[elIdx] = find_median(neighbourhood, filterSize*filterSize);
-		}
-	}
-	
-    tstop = second();
-    // std::cout << "sequentialMedian, " << tstop-tstart << std::endl;
 }
 
 void parallelMedian(std::vector<int> &output, std::vector<int> &input)
@@ -197,47 +76,11 @@ void parallelMedian(std::vector<int> &output, std::vector<int> &input)
 
 int main(int argc, char** argv)
 {
-    std::vector<int> seqInput(NROWS*NCOLS);
     std::vector<int> parInput(NROWS*NCOLS);
-    for(size_t i = 0; i < NROWS*NCOLS; ++i) {
-		seqInput[i] = parInput[i] = i;
-	}
-    std::vector<int> seqOutput(seqInput.size());
     std::vector<int> parOutput(parInput.size());
-
-#ifdef OUTPUT
-	FILE *outfile;
-    outfile = fopen("median2Dtest.txt","w");
-    fprintf(outfile,"Input: ");
-    for (size_t i = 0; i<NROWS*NCOLS; i++) {
-		if (i%NCOLS == 0) fprintf(outfile," \n");
-		fprintf(outfile,"%d, ", seqInput[i]);
-    }
-#endif
-	
-	sequentialMedian(seqOutput, seqInput);
+    for(size_t i = 0; i < NROWS*NCOLS; ++i) {
+	    parInput[i] = i;
+	}	
 	parallelMedian(parOutput, parInput);
-	
-#ifdef OUTPUT
-    fprintf(outfile,"\nSequential Output: ");
-    for (size_t i = 0; i<NROWS*NCOLS; i++) {
-		if (i%NCOLS == 0) fprintf(outfile," \n");
-		fprintf(outfile,"%d, ", seqOutput[i]);
-    }
-	fprintf(outfile,"\n");
-    fprintf(outfile,"\nParallel Output: ");
-    for (size_t i = 0; i<NROWS*NCOLS; i++) {
-		if (i%NCOLS == 0) fprintf(outfile," \n");
-		fprintf(outfile,"%d, ", parOutput[i]);
-    }
-	fprintf(outfile,"\n");
-	fclose(outfile);
-
-	if (compareResult(seqOutput, parOutput))
-		std::cout << "out is the same as in" << std:: endl;
-	else
-		std::cout << "ERROR: out != in" << std::endl;
-    #endif
-	
 	return 0;
 }
---
+++ stencil_code/ShengExamples/Sum3D/seqSum3D.cpp
@@ -0,0 +1,94 @@
+/**
+ * g++ seqSum3D.cpp -std=c++11 -O2 -lpthread -DRADIUS=1 -DNXS=3 -DNYS=4 -DNZS=5 -DPADDING=0 -DNITERS=4 -o seqSum3D
+ * ./seqSum3D
+ */
+
+#include <cassert>
+#include <sys/time.h>
+#include <vector>
+#include <iostream>
+#include <utility>
+#include <stdexcept>
+
+#define WRAP_AROUND 0
+#define FIXED_VALUE 1
+#define REPLICATE_LAST_ELEMENT 2
+
+double second()
+{
+	struct timeval tp;
+	struct timezone tzp;
+	int i;
+	i = gettimeofday(&tp,&tzp);
+	return ( (double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
+}
+
+void sequentialSum(std::vector<int> &output, std::vector<int> &input)
+{
+	double tstart, tstop;
+    tstart = second();
+
+	// sum
+	int nItems = NXS*NYS*NZS;
+	for (int iter=0; iter<NITERS; iter++)
+	{
+		// NEW
+		if (iter>0)
+		{
+			auto temp = input;
+		    input = output;
+			output = temp;
+		}
+
+		for (int elIdx=0; elIdx<nItems; ++elIdx)
+		{
+			int elx = elIdx % NXS;
+			int ely = (elIdx / NXS) % NYS;
+			int elz = elIdx / NXS / NYS;
+			int neighbourx, neighboury, neighbourz;
+			int filterx, filtery, filterz;
+			int filterSize = 2*RADIUS+1;
+			int sum=0;
+			
+			for (int filterIdx=0; filterIdx<filterSize*filterSize*filterSize; ++filterIdx) { // iterate over filter window, loop colaescing 
+				filterz = filterIdx / (filterSize*filterSize);
+				filtery = filterIdx % (filterSize*filterSize) / filterSize;
+				filterx = filterIdx % filterSize;
+				switch (PADDING)
+				{
+				    case WRAP_AROUND:
+					{
+						neighbourx = (elx+filterx-RADIUS+NXS)%NXS;
+						neighboury = (ely+filtery-RADIUS+NYS)%NYS;
+						neighbourz = (elz+filterz-RADIUS+NZS)%NZS;
+						sum += input[neighbourx+NXS*(neighboury+NYS*neighbourz)];
+						break;
+					}
+				    case FIXED_VALUE:
+					break;
+				    case REPLICATE_LAST_ELEMENT:
+					break;
+				default:
+					throw std::invalid_argument("Invalid padding option.");
+					break;
+				}
+			}
+			output[elIdx] = sum;
+		}
+	}
+	
+    tstop = second();
+    std::cout << tstop-tstart << ", seq, 0, "<< NXS*NYS*NZS <<  std::endl;
+}
+
+int main (int argc, char** argv)
+{
+    std::vector<int> seqInput(NXS*NYS*NZS);
+    std::vector<int> seqOutput(seqInput.size());
+    for(size_t i=0; i<NXS*NYS*NZS; ++i) {
+		seqInput[i] = i;
+	}
+	sequentialSum(seqOutput, seqInput);
+	return 0;
+}
+
