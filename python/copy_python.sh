65e62d8763d649077f66f0a094d1cde9285b47c9
shengsli <sheng.s.li@outlook.com>
2019-08-02 19:53:49 +0100

    rm csv pdf; add test script

---
+++ python/copy_python.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cp plot.py ~/stencil/stencil_code_v0/ShengExamples/Sum1D
+cp plot.py ~/stencil/stencil_code_v0/ShengExamples/Median1D
+
+cp plot.py ~/stencil/stencil_code_v0/ShengExamples/Sum2D
+cp plot.py ~/stencil/stencil_code_v0/ShengExamples/Median2D
+cp plot.py ~/stencil/stencil_code_v0/ShengExamples/HeatTransfer2D
+cp plot.py ~/stencil/stencil_code_v0/ShengExamples/GaussianBlur2D
+
+cp plot.py ~/stencil/stencil_code_v0/ShengExamples/Sum3D
+cp plot.py ~/stencil/stencil_code_v0/ShengExamples/Median3D
